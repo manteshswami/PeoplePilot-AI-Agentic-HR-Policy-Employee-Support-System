94c9af326d06fa93cf6daa972a77ce15b1580564
Mantesh Swami <swamimantesh215@gmail.com>
2026-09-20 20:22:48 +0530

    Data Ingestion ✔️

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-from app.core.config import get_settings
+from app.services.ingestion import load_file, Chunk_documents
+from pathlib import Path
+docs = load_file(Path("data/sample_kb/company_hr_handbook.md"))
+chunked_docs = Chunk_documents(docs)
 
-settings = get_settings()
-
-print(f"App Name: {settings.app_name}")
+print(chunked_docs)
